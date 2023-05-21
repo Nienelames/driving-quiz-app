ba2aba6dfe57ee4a9d518ee0856a552dfcbfd46f
Karolis Rauckis <k.rauckis@gmail.com>
2023-05-21 22:45:29 +0300

    Rename whitelist functions

---
+++ DrivingQuizApp/Server/Contracts/IAccountRepository.cs
@@ -0,0 +1,12 @@
+using DrivingQuizApp.Server.DTOs.Requests;
+
+namespace DrivingQuizApp.Server.Contracts;
+
+public interface IAccountRepository
+{
+    Task AddAsync(int userId);
+    Task WhitelistAsync(int userId);
+    Task BlacklistAsync(int userId);
+    Task AddPermissionsAsync(UserPermissionRequestDto userPermissionRequestDto);
+    Task UpdatePermissionsAsync(UserPermissionRequestDto userPermissionRequestDto);
+}
